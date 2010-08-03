23ec6b5ba0f34820ed891d3ff67fc7848b2ce269
Mike Dvorscak <officiallooking@gmail.com>
2010-08-03 15:44:44 -0500

    minor: ruby stylings

--- link_checker.rb
+++ link_checker.rb
@@ -3,28 +3,29 @@ require 'uri'
 require File.dirname(__FILE__)+ '/logger'
 class LinkChecker
 
-	def initialize(mode="production")
-		@log=Logger.new(mode,"link_checker")
+	def initialize(mode = "production")
+		@log = Logger.new(mode, "link_checker")
 	end
 
 	def response_code(uri_string)
 		#Without http:// at the beginning of the url, all requests fail.
 		#Make sure to add it if it isn't in there.
-		http_match=uri_string.match(/^http:\/\//)
-		uri_string="http://"+uri_string if http_match.nil?
+		http_match = uri_string.match(/^http:\/\//)
+		uri_string = "http://"+uri_string unless http_match
 
 		
-		uri=URI.parse(uri_string)
+		uri = URI.parse(uri_string)
 		begin
-		Net::HTTP.start(uri.host,uri.port){|http| 
-			response= http.head(uri.path.size > 0 ? uri.path : "/")
-			rc=response.code.to_i
-			@log.log(uri_string+" yeilded "+rc.to_s+" "+
-				    translate_code(rc))
-			rc
-		}
+		Net::HTTP.start(uri.host, uri.port) do |http| 
+			response = http.head(uri.path.size > 0 ? uri.path : "/")
+			@rc = response.code.to_i
+			@log.log(uri_string+" yeilded "+@rc.to_s+" "+
+				    translate_code(@rc))
+			@rc
+    end
 		rescue
 			@log.log("Invalid url entered: "+uri_string)
+      nil
 		end
 	end
 
@@ -32,22 +33,22 @@ class LinkChecker
 	def translate_code(code)
 		type=code/100
 		case type
-		when 1
-			return "Information"
-		when 2
-			return "Success"
-		when 3
-			return "Redirection"
-		when 4
-			return "Client Error"
-		when 5
-			return "Server Error"
+		when 1 : "Information"
+		when 2 : "Success"
+		when 3 : "Redirection"
+		when 4 : "Client Error"
+		when 5 : "Server Error"
 		else
-			return "Unknown Error"
+      "Unknown Error"
 		end
 	end
 
 	def good_link?(url)
-		return response_code(url)==200
+		response_code(url) == 200
 	end
+
+  def full_response(url)
+    response_code(url)
+    @rc.to_s+": "+translate_code(@rc)
+  end
 end
